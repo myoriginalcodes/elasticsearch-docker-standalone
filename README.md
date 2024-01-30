130747af2ed80dbf9b59a48db5930eccc861fd6c
Edson Nascimento <edna@boozt.com>
2024-01-30 17:10:33 +0100

    Add new samples, improve samples importing

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
-# elasticsearch-studies
-Repository created to study elasticsearch environment
+# elasticsearch-sharetheco-de
 
-Create sample data
+This repository was created to study the elasticsearch stack
 
-docker exec elasticsearch-sharetheco-de bash /usr/share/elasticsearch/scripts/import-sample.sh
+How to create the sample data
+
+Note: If you run the command below more than once, the index creation will fail
+as elasticsearch do not allows duplicated indexes. The data it self will be duplicated.
+So to avoid "dirty" data, make sure to run this command only once.
+
+docker exec elasticsearch-sharetheco-de bash /usr/share/elasticsearch/scripts/import-samples.sh
--- docker/elasticsearch/scripts/import-samples.sh
+++ docker/elasticsearch/scripts/import-samples.sh
@@ -5,3 +5,9 @@ cd /usr/share/elasticsearch/samples/shakespeare
 curl -H "Content-Type: application/json" -u "elastic:${ELASTIC_PASSWORD}" -XPUT "127.0.0.1:${ELASTIC_TCP_PORT}/shakespeare" --data-binary @shakespeare_mapping.json
 
 curl -H "Content-Type: application/json" -u "elastic:${ELASTIC_PASSWORD}" -XPUT "127.0.0.1:${ELASTIC_TCP_PORT}/shakespeare/_bulk" --data-binary @shakespeare_data.json
+
+cd /usr/share/elasticsearch/samples/movies
+
+curl -H "Content-Type: application/json" -u "elastic:${ELASTIC_PASSWORD}" -XPUT "127.0.0.1:${ELASTIC_TCP_PORT}/movies" --data-binary @movies_mapping.json
+
+curl -H "Content-Type: application/json" -u "elastic:${ELASTIC_PASSWORD}" -XPUT "127.0.0.1:${ELASTIC_TCP_PORT}/movies/_bulk" --data-binary @movies_data.json
